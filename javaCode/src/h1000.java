16f472e42349bfdb417eb0b5ec91c1a845f19fc1
hbghhy <hbghhy@126.com>
2016-11-02 16:50:01 +0800

    1000

---
+++ javaCode/src/h1000.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by hanyuhuang on 2016/11/2.
+ */
+public class h1000 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a,b;
+        while(scanner.hasNext()){
+            a=scanner.nextInt();
+            b=scanner.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
---
+++ javaCode/src/h1032.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hanyuhuang on 2016/11/2.
+ */
+public class h1032 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
