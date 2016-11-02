c06c0fde687869cea4af85533e7ae68ee741447e
hhy <hbghhy@126.com>
2016-11-02 21:44:25 +0800

    Rejected commit: Default

--- javaCode/src/h1032.java
+++ javaCode/src/h1032.java
@@ -1,8 +1,54 @@
+import java.util.Scanner;
+
 /**
  * Created by hanyuhuang on 2016/11/2.
  */
 public class h1032 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        while(n-->0){
+            String s=addSpecialChar(scanner.next(),'#');
+            int[] p=new int[s.length()];
+            p[0]=1;
+            int maxId=1,maxCenter=0;
+            int ans=0;
+            for(int i=1;i<p.length;i++){
+                if(i>=maxId){
+                    int j;
+                    for(j=1;i+j<p.length && i-j>=0 && s.charAt(i+j)==s.charAt(i-j) ;j++){
+
+                    }
+                    p[i]=j;
+                }else{
+                    int left=2*maxCenter-i;
+                    if(left>=0 &&  p[left]<maxId-i){
+                        p[i]=p[left];
+                    }else{
+                        int j;
+                        for(j=maxId-i;i+j<p.length && i-j>=0 && s.charAt(i+j)==s.charAt(i-j) ;j++){
+
+                        }
+                        p[i]=j;
+                    }
+                }
+                if(p[i]+i>=maxId){
+                    maxCenter=i;
+                    maxId=p[i]+i;
+                }
+                if(p[i]-1>ans) {
+                    ans=p[i]-1;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+    public static String addSpecialChar(String s, char c){
+        StringBuilder sb=new StringBuilder();
+        sb.append(c);
+        for (int i=0;i<s.length();i++){
+            sb.append(s.charAt(i)).append(c);
+        }
+        return sb.toString();
     }
 }
